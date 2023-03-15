3faccf696e956213a721e8c3e7b88547e71aaedf
Dr. Andreas Plagens <andreas@plagens.eu>
2023-03-15 09:38:27 +0100

    Initial Commit

---
+++ TabbedInterface/TabbedInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabbedInterfaceApp.swift
+//  TabbedInterface
+//
+//  Created by Dr. Andreas Plagens on 15.03.23.
+//
+
+import SwiftUI
+
+@main
+struct TabbedInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
